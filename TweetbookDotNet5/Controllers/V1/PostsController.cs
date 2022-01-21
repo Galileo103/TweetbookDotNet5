d3a032c0f107437d71355d5d3d0505aaccdbd0c6
Galal Shaban <galal.shaban@gameball.co>
2022-01-21 03:10:24 +0200

    Implementing native versioning (not best way)

---
+++ TweetbookDotNet5/Controllers/V1/PostsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TweetbookDotNet5.Contracts;
+using TweetbookDotNet5.Domain;
+
+namespace TweetbookDotNet5.Controllers.V1
+{
+    public class PostsController : Controller
+    {
+        private List<Post> _posts;
+
+        public PostsController()
+        {
+            _posts = new List<Post>();
+            for (int i = 0; i < 5; i++)
+            {
+                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
+            }
+        }
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+    }
+}
